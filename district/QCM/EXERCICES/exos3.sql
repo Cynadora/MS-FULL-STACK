64ae9b2dfb516c03364904fb2de75d4741dc06cf
Cynadora <sylvie.muzeaux@gmail.com>
2023-06-12 13:33:51 +0200

    modif exos3

--- district/QCM/EXERCICES/exos3.sql
+++ district/QCM/EXERCICES/exos3.sql
@@ -1,31 +1,31 @@
 --REQUETES D'INTERROGATION DE LA BDD
 
-    1--ok Afficher la liste des commandes ( la liste doit faire apparaitre la date, les informations du client, le plat et le prix )
+    1--Afficher la liste des commandes ( la liste doit faire apparaitre la date, les informations du client, le plat et le prix )
 SELECT date_commande, nom_client, telephone_client, email_client, adresse_client, libelle, total
 FROM commande 
 JOIN plat  ON id_plat = plat.id;
 
 
-    2--ok Afficher la liste des plats en spécifiant la catégorie
+    2--Afficher la liste des plats en spécifiant la catégorie
     SELECT plat.libelle, categorie.libelle
     FROM plat
     JOIN categorie ON plat.id_categorie = categorie.id;
 
-    3--ok Afficher les catégories et le nombre de plats actifs dans chaque catégorie
+    3--Afficher les catégories et le nombre de plats actifs dans chaque catégorie
 
     SELECT COUNT(active)
     FROM plat
     
 
 
-    4--ok Liste des plats les plus vendus par ordre décroissant
+    4--Liste des plats les plus vendus par ordre décroissant
     SELECT description
     FROM plat
     ORDER BY prix DESC;
 
 
 
-    5--okLe plat le plus rémunérateur
+    5--Le plat le plus rémunérateur
     SELECT libelle
     FROM plat
      -- Tri des plats par le prix du + cher au - cher
@@ -35,7 +35,7 @@ JOIN plat  ON id_plat = plat.id;
     
 
 
-6--okListe des clients et le chiffre d'affaire généré par client (par ordre décroissant)
+6--Liste des clients et le chiffre d'affaire généré par client (par ordre décroissant)
 SELECT commande.nom_client, SUM(commande.total) AS chiffre_affaire
                    
 FROM commande
@@ -44,7 +44,7 @@ ORDER BY total DESC;
 
 --ECRIRE DES REQUETES DE MODIFICATION DE LA BDD
     
-1--ok Ecrivez une requête permettant de supprimer les plats non actif de la base de données    
+1--Ecrivez une requête permettant de supprimer les plats non actif de la base de données    
 --sélectionne les plats spécifiques à supprimer (valeur 0 pour les plats non actifs)
 DELETE FROM plat
 WHERE plat.active = 'yes';
@@ -58,8 +58,10 @@ WHERE etat = 'livrée';
 INSERT INTO categorie (libelle, image, active)
 VALUES ('Kebab', 'kebab.jpg', 'Yes');
 
+INSERT INTO `plat`(`libelle`, `description`, `prix`, `image`, `id_categorie`, `active`) VALUES ('nouv','nouv',8,'nouv.jpg','20','No');
 
-4--/////Ecrivez une requête permettant d'augmenter de 10% le prix des plats de la catégorie 'Pizza'
+
+4--Ecrivez une requête permettant d'augmenter de 10% le prix des plats de la catégorie 'Pizza'
 UPDATE plat
 SET prix = prix * 1.1
 WHERE id_categorie = (SELECT id 
